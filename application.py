08f7c176c3e29fcf6f1be1588ff6e425f0ea5619
satishprasad1998 <s.prasad@iitg.ac.in>
2023-08-22 17:28:07 +0530

    application name chenged

--- application.py
+++ application.py
@@ -5,7 +5,8 @@ import pandas as pd
 from mlProject.pipeline.prediction import PredictionPipeline
 
 
-app = Flask(__name__) # initializing a flask app
+application = Flask(__name__) # initializing a flask app
+app=application
 
 @app.route('/',methods=['GET'])  # route to display the home page
 def homePage():
